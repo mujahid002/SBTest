7b279aabb271de411a6a39422c19081c41a1423f
Mujahid002 <mujahidshaik2002@gmail.com>
2024-02-09 12:39:21 +0530

    need documentation and testing

--- contracts/RedWallet.sol
+++ contracts/RedWallet.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.0;
 
-import {ERC721Enumerable, ERC721} from "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {ERC721URIStorage} from "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {AccessControl} from "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract SoulboundToken is ERC721, ERC721URIStorage, AccessControl {
+error RedWallet__CannotBurnSoulBoundToken();
+error RedWallet__CannotTransferSoulBoundToken();
+
+contract RedWallet is ERC721Enumerable, ERC721URIStorage, AccessControl, Pausable {
     bytes32 public constant REDWALLET_MINTER_ROLE =
         keccak256("REDWALLET_MINTER_ROLE");
     bytes32 public constant REDWALLET_BURNER_ROLE =
         keccak256("REDWALLET_BURNER_ROLE");
 
-    uint256 private _tokenIdCounter;
-    mapping(uint256 => bool) private s_soulBoundTokens;
+    uint256 public _tokenIdCounter;
 
-    event Attest(address indexed to, uint256 indexed tokenId);
-    event Revoke(address indexed to, uint256 indexed tokenId);
+    event transfer(address indexed from, address indexed to, uint256 tokenId, bytes data);
+
 
     constructor() ERC721("RedWalletSBT", "RSBT") {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -23,36 +27,45 @@ contract SoulboundToken is ERC721, ERC721URIStorage, AccessControl {
         _grantRole(REDWALLET_BURNER_ROLE, _msgSender());
     }
 
-    function safeMint(address to, string memory uri)
-        public
-        onlyRole(REDWALLET_MINTER_ROLE)
-    {
+    function safeMint(address to, string memory uri) public onlyRole(REDWALLET_MINTER_ROLE) whenNotPaused{
         _safeMint(to, _tokenIdCounter);
         _setTokenURI(_tokenIdCounter, uri);
-        _tokenIdCounter += 1;
+        _tokenIdCounter+=1;
     }
 
-    function burn(uint256 tokenId) external {
-        require(ownerOf(tokenId) == msg.sender, "Only owner of the token can burn it");
+    function burn(uint256 tokenId) external onlyRole(REDWALLET_BURNER_ROLE) whenNotPaused{
+        if(ownerOf(tokenId)==_msgSender()) revert RedWallet__CannotBurnSoulBoundToken();
         _burn(tokenId);
     }
 
-
-    function _beforeTokenTransfer(address from, address to, uint256) pure override internal {
-        require(from == address(0) || to == address(0), "Not allowed to transfer token");
+    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal virtual override {
+        emit transfer(from, to,tokenId,data);
+        revert RedWallet__CannotTransferSoulBoundToken();
     }
 
-    function _afterTokenTransfer(address from, address to, uint256 tokenId) override internal {
-
-        if (from == address(0)) {
-            emit Attest(to, tokenId);
-        } else if (to == address(0)) {
-            emit Revoke(to, tokenId);
-        }
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override(ERC721, IERC721) {
+        emit transfer(from, to,tokenId,data);
+        revert RedWallet__CannotTransferSoulBoundToken();
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721) {
-        super._burn(tokenId);
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override(ERC721, IERC721) {
+        emit transfer(from, to,tokenId,"");
+        revert RedWallet__CannotTransferSoulBoundToken();
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
     }
 
     function tokenURI(uint256 tokenId)
@@ -64,16 +77,11 @@ contract SoulboundToken is ERC721, ERC721URIStorage, AccessControl {
         return super.tokenURI(tokenId);
     }
 
-    function getTokenCount() public view returns(uint256){
-        return _tokenIdCounter;
-    }
-
     function supportsInterface(bytes4 interfaceId)
         public
         view
         virtual
-        override(ERC721, ERC721URIStorage, AccessControl)
-        onlyRole(DEFAULT_ADMIN_ROLE)
+        override( ERC721Enumerable,ERC721URIStorage, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
